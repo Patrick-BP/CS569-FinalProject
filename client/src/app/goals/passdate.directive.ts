19ae3716535b66a5b31ba8cab584b7d0bccf66dc
Patrick <papybp@gmail.com>
2022-11-20 12:13:07 -0600

    gdgfdgf

--- client/src/app/goals/passdate.directive.ts
+++ client/src/app/goals/passdate.directive.ts
@@ -4,7 +4,7 @@ import { Directive, HostBinding, Input, OnInit } from '@angular/core';
   selector: '[appPassDate]'
 })
 export class PassDateDirective implements OnInit {
-@Input('appPassDate') currentDate!:string;
+@Input('appPassDate') stepDeadline!:string;
 @Input('stepstatus') status!:string;
 @HostBinding('style.color') color:string="black"
 @HostBinding('attr.src') src:string="/assets/dot.png"
@@ -14,8 +14,8 @@ export class PassDateDirective implements OnInit {
   }
   ngOnInit(): void { 
     
-    const date = new Date().toISOString().slice(0, 10);
-   if((new Date(date).getTime() > new Date(this.currentDate).getTime()) && this.status !=="completed"){
+    const toDayDate = new Date().toISOString().slice(0, 10);
+   if((new Date(toDayDate).getTime() > new Date(this.stepDeadline).getTime()) && this.status !=="completed"){
       this.color = "red";
       this.src = "/assets/Blinking_warning.gif";
    }
